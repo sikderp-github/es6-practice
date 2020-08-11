a16d234c0b4417a4a404dbd7c83f16453b2a871d
sikderp-github <66568657+sikderp-github@users.noreply.github.com>
2020-08-11 21:43:58 +0600

    template final

---
+++ template.js
@@ -0,0 +1,17 @@
+const firstName = 'Rahim';
+const lastName = "Uddin";
+const fullName = firstName + " " + lastName;
+const fullName2 = ` ${firstName} ${lastName} ${20+30+50} is a good boy.`
+console.log(fullName);
+console.log(fullName2);
+
+const multiLine = "I love you\n" +
+    'I miss you\n' +
+    'I feel you';
+
+console.log(multiLine);
+
+const multiLine2 = `I love you
+I miss you
+But I don't feel you now.`
+console.log(multiLine2);
