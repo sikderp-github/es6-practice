3baf8173f33dbe87e5ab579f7eb3c954b01e0abb
sikderp-github <66568657+sikderp-github@users.noreply.github.com>
2020-08-14 00:24:35 +0600

    scope practice

---
+++ scope.js
@@ -0,0 +1,24 @@
+let bonus = 20;
+
+function sum(first, second) {
+    let result = first + second + bonus;
+    console.log(result);
+    // console.log(bonus);
+    if (result > 9) {
+        let mood = "Happy";
+        mood = "fishy";
+        mood = "funcky";
+
+        console.log(mood);
+    }
+    // console.log(mood);
+    return result;
+}
+
+const output = sum(13, 17);
+// console.log(bonus);
+console.log(output);
+
+
+let day = "friday";
+console.log(day);
