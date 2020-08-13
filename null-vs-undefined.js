c828481a0f6af3bed026424f88148c907b431290
sikderp-github <66568657+sikderp-github@users.noreply.github.com>
2020-08-13 23:34:54 +0600

    Null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,30 @@
+let pakhi;
+console.log(pakhi);
+
+function add(num1, num2) {
+    console.log(num1 + num2);
+    return;
+}
+
+const result = add(13, 17);
+console.log(result);
+
+function add(num1, num2) {
+    console.log(num1, num2);
+    return;
+}
+
+const result = add(13);
+console.log(result);
+
+const premik = {
+    name: 'Smart Boy',
+    phone: 454864
+};
+console.log(premik.gf);
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [2, 5, 9];
+console.log(ages[5]);
